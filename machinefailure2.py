d921e937f64138f865c97fcf131b3f6da1549ba4
JiaJinLaw <156111071+JiaJinLaw@users.noreply.github.com>
2024-01-20 11:04:29 +0800

    Update machinefailure2.py

--- machinefailure2.py
+++ machinefailure2.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1QubduNG4VUCcHpqZyz6h3PERuTtVn-sh
 """
 
-pip install streamlit
 
 import streamlit as st
 import pandas as pd
